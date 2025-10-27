9b9d4239c6498e4bfb7ffc54d82e584b7825dc93
DHARANI NALABOLU <52799460+dharani0804@users.noreply.github.com>
2025-10-27 20:26:39 +0000

    Add homework-5 folder and myproject files

---
+++ homework 5/myproject/main.py
@@ -0,0 +1,20 @@
+import pickle
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Load the trained pipeline
+with open("pipeline_v1.bin", "rb") as f:
+    pipeline = pickle.load(f)
+
+class Lead(BaseModel):
+    lead_source: str
+    number_of_courses_viewed: int
+    annual_income: float
+
+app = FastAPI()
+
+@app.post("/predict")
+def predict(lead: Lead):
+    lead_dict = lead.dict()
+    prob = pipeline.predict_proba([lead_dict])[0][1]
+    return {"conversion_probability": prob}
